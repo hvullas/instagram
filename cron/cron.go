52cca6df87c12a42a99c66c7c2ebf6e65e41644e
hvullas <ullashv21@gmail.com>
2023-10-17 11:14:45 +0530

    reconfigured the project structure for modularity

---
+++ cron/cron.go
@@ -0,0 +1,27 @@
+package cron
+
+import (
+	"backend/db"
+	"log"
+
+	"github.com/robfig/cron/v3"
+)
+
+func RunCron() {
+	//cron delete stories after 24 hours
+
+	//cron delete posts which are not updated with media
+	cron := cron.New()
+
+	cron.AddFunc("29 16 * * *", func() {
+
+		//_,err=db.Query("DELETE FROM posts WHERE complete_post=$1",false)
+		_, err = db.DB.Query("DELETE FROM example WHERE timestamp<= current_timestamp - interval '10 minute'")
+		if err != nil {
+			log.Panic(err)
+			return
+		}
+		log.Println("cron active")
+	})
+	cron.Start()
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module postgresDB
+module backend
 
 go 1.20
 
 require (
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/robfig/cron/v3 v3.0.1
 	golang.org/x/crypto v0.11.0
